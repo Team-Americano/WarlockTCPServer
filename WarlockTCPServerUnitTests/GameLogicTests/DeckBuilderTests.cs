5676466c65f4bf53cd9251b8d95a0c01b7a756eb
Peyton-Cysewski <github.peyton.cysewski@gmail.com>
2020-09-05 04:24:57 -0700

    added DeckBuilder tests

--- WarlockTCPServerUnitTests/GameLogicTests/DeckBuilderTests.cs
+++ WarlockTCPServerUnitTests/GameLogicTests/DeckBuilderTests.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 using WarlockTCPServer.Builders;
 using static WarlockTCPServer.Constants.DeckPresets;
+using System.Collections;
+using System.Diagnostics;
 
 namespace WarlockTCPServerUnitTests.GameLogicTests
 {
@@ -13,10 +16,50 @@ namespace WarlockTCPServerUnitTests.GameLogicTests
             var deckBuilder = new DeckBuilder();
             var deck = deckBuilder.Build("BaseDeck");
 
+            Dictionary<string, int> tracker = new Dictionary<string, int>
+            {
+                { "Warden", 0 },
+                { "Brute", 0 },
+                { "Enchanter", 0 },
+                { "Sorcerer", 0 },
+                { "Trapper", 0 },
+                { "Stalker", 0 },
 
+                { "Undead", 0 },
+                { "Glacial", 0 },
+                { "Infernal", 0 },
+                { "Bestial", 0 },
+                { "Aberrant", 0 },
+                { "Aquatic", 0 },
+
+                { "Common", 0 },
+                { "Uncommon", 0 },
+                { "Rare", 0 }
+            };
+            foreach (var card in deck)
+            {
+                tracker[card.Class]++;
+                tracker[card.Origin]++;
+                tracker[card.Rarity]++;
+            }
 
             Assert.NotNull(deck);
             Assert.Equal(108, deck.Length);
+            Assert.Equal(18, tracker["Warden"]);
+            Assert.Equal(27, tracker["Brute"]);
+            Assert.Equal(12, tracker["Enchanter"]);
+            Assert.Equal(18, tracker["Sorcerer"]);
+            Assert.Equal(15, tracker["Trapper"]);
+            Assert.Equal(18, tracker["Stalker"]);
+            Assert.Equal(18, tracker["Undead"]);
+            Assert.Equal(18, tracker["Glacial"]);
+            Assert.Equal(18, tracker["Infernal"]);
+            Assert.Equal(18, tracker["Bestial"]);
+            Assert.Equal(18, tracker["Aberrant"]);
+            Assert.Equal(18, tracker["Aquatic"]);
+            Assert.Equal(54, tracker["Common"]);
+            Assert.Equal(36, tracker["Uncommon"]);
+            Assert.Equal(18, tracker["Rare"]);
         }
 
         [Fact]
@@ -25,10 +68,50 @@ namespace WarlockTCPServerUnitTests.GameLogicTests
             var deckBuilder = new DeckBuilder();
             var deck = deckBuilder.Build(BaseDeck);
 
+            Dictionary<string, int> tracker = new Dictionary<string, int>
+            {
+                { "Warden", 0 },
+                { "Brute", 0 },
+                { "Enchanter", 0 },
+                { "Sorcerer", 0 },
+                { "Trapper", 0 },
+                { "Stalker", 0 },
 
+                { "Undead", 0 },
+                { "Glacial", 0 },
+                { "Infernal", 0 },
+                { "Bestial", 0 },
+                { "Aberrant", 0 },
+                { "Aquatic", 0 },
+
+                { "Common", 0 },
+                { "Uncommon", 0 },
+                { "Rare", 0 }
+            };
+            foreach (var card in deck)
+            {
+                tracker[card.Class]++;
+                tracker[card.Origin]++;
+                tracker[card.Rarity]++;
+            }
 
             Assert.NotNull(deck);
             Assert.Equal(108, deck.Length);
+            Assert.Equal(18, tracker["Warden"]);
+            Assert.Equal(27, tracker["Brute"]);
+            Assert.Equal(12, tracker["Enchanter"]);
+            Assert.Equal(18, tracker["Sorcerer"]);
+            Assert.Equal(15, tracker["Trapper"]);
+            Assert.Equal(18, tracker["Stalker"]);
+            Assert.Equal(18, tracker["Undead"]);
+            Assert.Equal(18, tracker["Glacial"]);
+            Assert.Equal(18, tracker["Infernal"]);
+            Assert.Equal(18, tracker["Bestial"]);
+            Assert.Equal(18, tracker["Aberrant"]);
+            Assert.Equal(18, tracker["Aquatic"]);
+            Assert.Equal(54, tracker["Common"]);
+            Assert.Equal(36, tracker["Uncommon"]);
+            Assert.Equal(18, tracker["Rare"]);
         }
     }
 }
