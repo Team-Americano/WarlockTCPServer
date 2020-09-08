aa381c78daf7da77ffd6390172d9bc537506f81c
Peyton-Cysewski <github.peyton.cysewski@gmail.com>
2020-09-08 12:49:48 -0700

    completed Targeter test

--- WarlockTCPServerUnitTests/GameLogicTests/ActorAppliedEffectTests.cs
+++ WarlockTCPServerUnitTests/GameLogicTests/ActorAppliedEffectTests.cs
@@ -2,13 +2,16 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using static WarlockTCPServer.Builders.ActorBuilder;
+using static WarlockTCPServer.Constants.DeckConstants;
+using static WarlockTCPServer.Constants.ActorAppliedEffects;
 
 namespace WarlockTCPServerUnitTests.GameLogicTests
 {
     public class ActorAppliedEffectTests
     {
         [Fact]
-        public void FirstAliveWorks()
+        public void StandardDamageWorks()
         {
 
         }
--- WarlockTCPServerUnitTests/GameLogicTests/ActorTargeterTests.cs
+++ WarlockTCPServerUnitTests/GameLogicTests/ActorTargeterTests.cs
@@ -1,7 +1,13 @@
 ﻿using Xunit;
 using System;
-using System.Collections.Generic;
 using System.Text;
+using System.Collections.Generic;
+using WarlockTCPServer.GameLogic.ActorComponents;
+using static WarlockTCPServer.Builders.ActorBuilder;
+using static WarlockTCPServer.Constants.DeckConstants;
+using static WarlockTCPServer.Constants.ActorTargeters;
+using Targeter = WarlockTCPServer.Constants.DeckConstants.Targeter;
+using System.Linq;
 
 namespace WarlockTCPServerUnitTests.GameLogicTests
 {
@@ -10,7 +16,18 @@ namespace WarlockTCPServerUnitTests.GameLogicTests
         [Fact]
         public void FirstAliveWorks()
         {
+            var enemy1 = Build(Characters.Skeleton, 1);
+            enemy1.Health.CurrentValue = 0;
+            var enemy2 = Build(Characters.Gazer, 2);
+            var enemy3 = Build(Characters.Revenant, 3);
+            var friendlies = new Actor[] { null };
+            var enemies = new Actor[] { enemy1, enemy2, enemy3 };
 
+            var targeted = Targeters[Targeter.FirstAlive](friendlies, enemies).ToArray();
+
+            Assert.NotNull(targeted);
+            Assert.Single(targeted);
+            Assert.Equal(enemy2, targeted[0]);
         }
     }
 }
